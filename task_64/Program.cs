4efc2419e5940aeefdd37dbbf4ff7483d8bd35d9
Sergey <seregad07@mail.ru>
2022-10-02 22:58:05 +0300

    Done

---
+++ task_64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число натуральное число N ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int End =  1;
+Console.WriteLine("Все натуральные числа от 1 до N: ");
+void Nat (int End, int Number)
+{
+    if (Number < End)
+
+    {
+        return;
+    }
+    else
+    {
+        Nat (End, Number  - 1);
+        Console.Write($"{Number}, ");
+
+    }
+}
+//         Number = Number - End;
+//         Console.Write (Number);
+//     }
+// }
+try
+{
+    Nat(End,Number);
+}
+catch 
+{
+    Console.WriteLine("oh...something wrong");
+}
---
+++ task_66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Sum(int M, int N)
+{
+    int result = M;
+    if (M>N) 
+    {
+        Console.WriteLine("число N должно быть больше M ");
+        return 0;
+    }
+    
+    if (M == N)
+        return 0;
+    else
+    {
+        return N+Sum(M,N-1);
+    }
+}
+try{
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма всех натуральных чисел в промежутке от M до N: ");
+Console.WriteLine(M+Sum(M,N));
+}
+catch 
+{
+    Console.WriteLine("oh...something wrong");
+}
